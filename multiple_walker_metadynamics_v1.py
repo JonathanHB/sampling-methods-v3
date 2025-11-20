convergence estimates have a bug. Need to separate error calculation code from individual methods

multiple_walker_metadynamics_v1.py
@@ -34,7 +34,7 @@ def parallel_trj_histogram_mtd(state, params):
     est_populations = np.exp(((kT+grid.dT)/grid.dT)*grid.grid)
     est_populations /= np.sum(est_populations)
 
-    maew = np.mean([spi*abs(espi-spi) for spi, espi in zip(pops_norm, est_populations)])*(len(binbounds)+1)
+    maew = np.mean([abs(espi-spi) for spi, espi in zip(pops_norm, est_populations)])
 
     t2 = time.time()
     #print(f"analysis={t2-t1}")
@@ -51,7 +51,7 @@ def parallel_trj_histogram_mtd(state, params):
     est_pops_hist = [ebp/len(trjs.flatten()) for ebp in est_bin_pops[0]]
 
     #calculate the weighted mean absolute error of the estimated bin populations
-    maew_hist = np.mean([spi*abs(espi-spi) for spi, espi in zip(pops_norm, est_pops_hist)])*(len(binbounds)+1)
+    maew_hist = np.mean([abs(espi-spi) for spi, espi in zip(pops_norm, est_pops_hist)])
 
     #----------------------------combined grid+histogram-based population estimation----------------------------#
 
@@ -62,7 +62,7 @@ def parallel_trj_histogram_mtd(state, params):
     # plt.show()
 
     #calculate the weighted mean absolute error of the estimated bin populations
-    maew_hist_weighted = np.mean([spi*abs(espi-spi) for spi, espi in zip(pops_norm, est_pops_hist_weighted)])*(len(binbounds)+1)
+    maew_hist_weighted = np.mean([abs(espi-spi) for spi, espi in zip(pops_norm, est_pops_hist_weighted)])
 
 
     # #----------------------------MSM-based population estimation----------------------------------#
@@ -79,7 +79,7 @@ def parallel_trj_histogram_mtd(state, params):
     eqp_msm = MSM_methods.transitions_to_eq_probs_v2(transitions, len(binbounds)+1, show_TPM=False)
     
     #calculate the weighted mean absolute error of the estimated bin populations
-    maew_msm = np.mean([spi*abs(espi-spi) for spi, espi in zip(pops_norm, eqp_msm)])*(len(binbounds)+1)
+    maew_msm = np.mean([abs(espi-spi) for spi, espi in zip(pops_norm, eqp_msm)])
 
 
     # plt.plot(grid.grid)
@@ -93,7 +93,7 @@ def parallel_trj_histogram_mtd(state, params):
 def sampler_parallel_hist_mtd(system, aggregate_simulation_limit, molecular_time_limit, save_period, n_timepoints, kT, dt, binbounds, bincenters):
 
     rate = 0.0000001*save_period
-    dT = 0.5
+    dT = 0.9
     stdev = [0.5]
 
     #determine number of parallel simulations and steps per simulation

parallel_simulations_v1.py
@@ -37,7 +37,7 @@ def parallel_trj_histogram(state, params):
     est_bin_pops_norm = [ebp/len(trjs.flatten()) for ebp in est_bin_pops[0]]
 
     #calculate the weighted mean absolute error of the estimated bin populations
-    maew = np.mean([spi*abs(espi-spi) for spi, espi in zip(pops_norm, est_bin_pops_norm)])*(len(binbounds)+1)
+    maew = np.mean([abs(espi-spi) for spi, espi in zip(pops_norm, est_bin_pops_norm)]) #*(len(binbounds)+1)
 
     #----------------------------MSM-based population estimation----------------------------------#
     
@@ -53,7 +53,7 @@ def parallel_trj_histogram(state, params):
     eqp_msm = MSM_methods.transitions_to_eq_probs_v2(transitions, len(binbounds)+1, show_TPM=False)
     
     #calculate the weighted mean absolute error of the estimated bin populations
-    maew_msm = np.mean([spi*abs(espi-spi) for spi, espi in zip(pops_norm, eqp_msm)])*(len(binbounds)+1)
+    maew_msm = np.mean([abs(espi-spi) for spi, espi in zip(pops_norm, eqp_msm)]) #*(len(binbounds)+1)
     
     t2 = time.time()
     #print(f"analysis={t2-t1}")

weighted_ensemble_v1.py
@@ -371,14 +371,14 @@ def we_histogram(state, params):
     est_bin_pops /= np.sum(est_bin_pops)  #normalize estimated bin populations
 
     #calculate the weighted mean absolute error of the estimated bin populations
-    maew = np.mean([spi*abs(espi-spi) for spi, espi in zip(bin_pops, est_bin_pops)])*(len(config_binner.binbounds)+1)
+    maew = np.mean([abs(espi-spi) for spi, espi in zip(bin_pops, est_bin_pops)]) #*(len(config_binner.binbounds)+1)
 
     #----------------------------MSM-based population estimation----------------------------#
 
     aggregate_transitions = np.concatenate([o[2] for o in observables], axis = 1).transpose()
     eqp_msm = MSM_methods.transitions_to_eq_probs_v2(aggregate_transitions, config_binner.n_bins, show_TPM=False)
 
-    maew_msm = np.mean([spi*abs(espi-spi) for spi, espi in zip(bin_pops, eqp_msm)])*(len(config_binner.binbounds)+1)
+    maew_msm = np.mean([abs(espi-spi) for spi, espi in zip(bin_pops, eqp_msm)]) #*(len(config_binner.binbounds)+1)
 
 
     return (x, e, w, cb, b, propagator, observables, cumulative_agg_t), (maew, est_bin_pops, maew_msm, eqp_msm, cumulative_agg_t), cumulative_agg_t >= aggregate_simulation_limit